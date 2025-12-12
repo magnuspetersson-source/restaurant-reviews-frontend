15530ac651295758d26e59573015011b355463fb
Magnus Petersson <magnus.petersson@icloud.com>
2025-12-12 12:09:15 +0100

    Remove Google Maps API key from repo config

--- js/config.js
+++ js/config.js
@@ -6,6 +6,4 @@ window.RR_CONFIG = {
   homeLat: 56.67526326918155,
   homeLng: 12.840977534456771,
 
-  // GOOGLE MAPS – frontend (publik, låst via referrer)
-  googleMapsApiKey: "AIzaSyDvkuMeVZtWDnIRvaR21_AO_7uB5Pendug",
 };
